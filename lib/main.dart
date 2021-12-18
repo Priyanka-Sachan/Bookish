49199e7cfdd36af560ea2154c61cffac8866cad6
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-12-18 19:15:07 +0530

    Add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookish/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,14 +9,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final theme = BookishTheme.light();
     return MaterialApp(
+      theme: theme,
       title: 'Bookish',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Bookish'),
+          title: Text('Bookish', style: theme.textTheme.headline6),
         ),
         body: Center(
-          child: Text('Bookish'),
+          child: Text('Bookish', style: theme.textTheme.headline1),
         ),
       ),
     );
