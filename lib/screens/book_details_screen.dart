51d46031be7cf7f79949d2dae7e1210c5e913216
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-12-24 15:23:50 +0530

    Book details ui

--- lib/screens/book_details_screen.dart
+++ lib/screens/book_details_screen.dart
@@ -8,6 +8,7 @@ class BookDetailsScreen extends StatelessWidget {
 
   BookDetailsScreen({Key? key, required this.book}) : super(key: key);
   List<String> tags = [];
+
   void getTags() {
     book.subjects.forEach((s) {
       s.split('--').forEach((e) {
@@ -21,129 +22,161 @@ class BookDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     getTags();
-    return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 8),
-        shrinkWrap: true,
-        children: [
-          Image.network(
-            '${book.formats['image/jpeg'] != null ? book.formats['image/jpeg']?.replaceAll('small', 'medium') : 'https://i.pinimg.com/736x/a6/50/cd/a650cdc389e72a5213be5f05a8fcd9db.jpg'}',
-            fit: BoxFit.cover,
-            width: MediaQuery.of(context).size.width,
-            height: 250,
-          ),
-          Text(
-            book.title,
-            style: Theme.of(context).textTheme.headline3,
-            softWrap: true,
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            book.authors != null && book.authors!.isNotEmpty
-                ? book.authors![0].name.split(',').length > 1
-                    ? ('${book.authors![0].name.split(',')[1]} ${book.authors![0].name.split(',')[0]}')
-                    : book.authors![0].name
-                : 'Anonymous',
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                ?.copyWith(color: Colors.grey),
-            softWrap: true,
-            textAlign: TextAlign.center,
-          ),
-          ...book.languages.map((l) => Text(' ${l.toUpperCase()}',
-              style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-              textAlign: TextAlign.center)),
-          Wrap(
-            spacing: 4,
-            alignment: WrapAlignment.center,
-            children: [
-              ...tags.map(
-                (tag) => ChoiceChip(
-                  padding: EdgeInsets.zero,
-                  label: Text(tag),
-                  shape: StadiumBorder(side: BorderSide()),
-                  labelStyle:
-                      TextStyle(color: Theme.of(context).colorScheme.secondary),
-                  selected: true,
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  onSelected: (_) {},
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Divider(
-            height: 2,
-            color: Colors.black,
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Icon(Icons.download),
-                  Text(
-                    book.downloadCount != 0
-                        ? book.downloadCount.toString() + '\nDOWNLOADS'
-                        : '--',
-                    style: Theme.of(context).textTheme.bodyText1,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('text/html'.toUpperCase(),
-                      semanticsLabel: book.formats['text/html']),
-                  Text('text/plain'.toUpperCase(),
-                      semanticsLabel: book.formats['text/plain']),
-                  Text('application/epub+zip'.toUpperCase(),
-                      semanticsLabel: book.formats['application/epub+zip']),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Divider(
-            height: 2,
-            color: Colors.black,
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          book.bookshelves.isNotEmpty
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      child: Scaffold(
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          children: [
+            ListView(
+              primary: false,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: [
+                Stack(
                   children: [
-                    Text(
-                      'If you liked ${book.title}'.toUpperCase(),
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline5
-                          ?.copyWith(wordSpacing: 0.1, letterSpacing: 0.2),
+                    Image.network(
+                      '${book.formats['image/jpeg'] != null ? book.formats['image/jpeg']?.replaceAll('small', 'medium') : 'https://i.pinimg.com/736x/a6/50/cd/a650cdc389e72a5213be5f05a8fcd9db.jpg'}',
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width,
+                      height: 300,
                     ),
-                    Text(
-                      'You can also checkout these bookshelves..',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline3
-                          ?.copyWith(color: Colors.grey),
-                    ),
-                    ...book.bookshelves.map((b) => _bookshelfCard(context, b))
+                    Positioned.fill(
+                        child: Container(
+                      decoration: BoxDecoration(color: Colors.black45),
+                    )),
+                    Positioned(
+                        top: 32,
+                        left: 8,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ))
                   ],
+                ),
+                Text(
+                  book.title,
+                  style: Theme.of(context).textTheme.headline3,
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  book.authors != null && book.authors!.isNotEmpty
+                      ? book.authors![0].name.split(',').length > 1
+                          ? ('${book.authors![0].name.split(',')[1]} ${book.authors![0].name.split(',')[0]}')
+                          : book.authors![0].name
+                      : 'Anonymous',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      ?.copyWith(color: Colors.grey),
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                ),
+                ...book.languages.map((l) => Text(' ${l.toUpperCase()}',
+                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                    textAlign: TextAlign.center)),
+                Wrap(
+                  spacing: 4,
+                  alignment: WrapAlignment.center,
+                  children: [
+                    ...tags.map(
+                      (tag) => ChoiceChip(
+                        padding: EdgeInsets.zero,
+                        label: Text(tag),
+                        shape: StadiumBorder(side: BorderSide()),
+                        labelStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary),
+                        selected: true,
+                        selectedColor: Theme.of(context).colorScheme.primary,
+                        onSelected: (_) {},
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.download),
+                        Text(
+                          book.downloadCount != 0
+                              ? book.downloadCount.toString() + '\nDOWNLOADS'
+                              : '--',
+                          style: Theme.of(context).textTheme.bodyText1,
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text('text/html'.toUpperCase(),
+                            semanticsLabel: book.formats['text/html']),
+                        Text('text/plain'.toUpperCase(),
+                            semanticsLabel: book.formats['text/plain']),
+                        Text('application/epub+zip'.toUpperCase(),
+                            semanticsLabel:
+                                book.formats['application/epub+zip']),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Icon(
+                  Icons.arrow_circle_down,
+                  size: 32,
                 )
-              : SizedBox()
-        ],
+              ],
+            ),
+            book.bookshelves.isNotEmpty
+                ? Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8, right: 8, top: 40, bottom: 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'If you liked ${book.title}'.toUpperCase(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(wordSpacing: 0.1, letterSpacing: 0.2),
+                        ),
+                        Text(
+                          'You can also checkout these bookshelves..',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline3
+                              ?.copyWith(color: Colors.grey),
+                        ),
+                        Expanded(
+                          child: ListView(
+                            padding: EdgeInsets.zero,
+                            primary: false,
+                            shrinkWrap: true,
+                            children: [
+                              ...book.bookshelves
+                                  .map((b) => _bookshelfCard(context, b))
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : SizedBox()
+          ],
+        ),
       ),
     );
   }
